039a6daf82bfc72493df8d47b3b0f6ae2b2a4c3a
TanukiTamer <tim.van.eerdewegh@telenet.be>
2016-02-04 16:58:04 +0100

    Hello World!

--- StringShenanigans/StringShenanigans/Program.cs
+++ StringShenanigans/StringShenanigans/Program.cs
@@ -10,6 +10,8 @@ namespace StringShenanigans
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
         }
     }
 }
